e090c880396d32beacb8d5a5c19004d7295f0200
Graccianadm <lucasgracianoprofissional@hotmail.com>
2025-06-29 14:39:50 -0300

    Create build.gradle.kts

---
+++ app/src/main/java/com/colormediumearth/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.colormediumearth"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.colormediumearth"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.material:material:1.11.0")
+}
